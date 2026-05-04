c0a1266a3e0638964bd150fdecc44fe362fcdffe
stevensitosava <jeusuarioestandar@gmail.com>
2026-05-04 18:49:12 +0200

    emails

--- api/contact.js
+++ api/contact.js
@@ -11,10 +11,15 @@ export default async function handler(req, res) {
     const { name, email, message } = req.body;
 
     await resend.emails.send({
-      from: "Contacto <contacto@miniik.com>",
-      to: "tuemail@gmail.com",
-      subject: `Nuevo mensaje de ${name}`,
-      html: `<p>${message}</p>`,
+        from: "MiniIk Contact <[EMAIL_ADDRESS]>",
+        to: "[EMAIL_ADDRESS]",
+        subject: `Nuevo mensaje de ${name}`,
+        reply_to: email,
+        html: `
+            <p><b>Nombre:</b> ${name}</p>
+            <p><b>Email:</b> ${email}</p>
+            <p><b>Mensaje:</b> ${message}</p>
+        `,
     });
 
     return res.status(200).json({ success: true });
